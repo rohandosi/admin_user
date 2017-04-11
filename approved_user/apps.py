501c997172adbebb70e547079156341d1bae5db9
Rohan Dosi <RohanDosi@Rohan.local>
2017-04-11 14:06:04 +0530

    basic login

---
+++ approved_user/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ApprovedUserConfig(AppConfig):
+    name = 'approved_user'
---
+++ approved_user/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url="login/")
+def home(request):
+    return render(request, "home.html")
+
+
